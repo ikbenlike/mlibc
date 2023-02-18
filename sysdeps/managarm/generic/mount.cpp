4c23e78b341671d85757485df57c77d599f75f2e
ikbenlike <thewatcheriswatchinu@gmail.com>
2023-02-18 16:31:17 +0100

    sysdeps/managarm: pass on mount 'data' argument in bragi request

--- sysdeps/managarm/generic/mount.cpp
+++ sysdeps/managarm/generic/mount.cpp
@@ -12,13 +12,16 @@
 namespace mlibc {
 
 int sys_mount(const char *source, const char *target,
-		const char *fstype, unsigned long, const void *) {
+		const char *fstype, unsigned long, const void *data) {
 	SignalGuard sguard;
 
 	managarm::posix::MountRequest<MemoryAllocator> req(getSysdepsAllocator());
 	req.set_path(frg::string<MemoryAllocator>(getSysdepsAllocator(), source));
 	req.set_target_path(frg::string<MemoryAllocator>(getSysdepsAllocator(), target));
 	req.set_fs_type(frg::string<MemoryAllocator>(getSysdepsAllocator(), fstype));
+	if(data){
+		req.set_arguments(frg::string<MemoryAllocator>(getSysdepsAllocator(), (char*)data));
+	}
 
 	auto [offer, send_head, send_tail, recv_resp] =
 		exchangeMsgsSync(
@@ -37,6 +40,8 @@ int sys_mount(const char *source, const char *target,
 	auto resp = *bragi::parse_head_only<managarm::posix::SvrResponse>(recv_resp, getSysdepsAllocator());
 	if(resp.error() == managarm::posix::Errors::FILE_NOT_FOUND)
 		return ENOENT;
+	else if(resp.error() == managarm::posix::Errors::UNKNOWN_FILESYSTEM)
+		return ENODEV;
 	__ensure(resp.error() == managarm::posix::Errors::SUCCESS);
 	return 0;
 }
